b8f5db4f3e69738ebfd72cb403d907a32d2609a8
supanat <supanat.ap@gmail.com>
2021-03-08 17:10:34 +0700

    a

--- 105_lab5.2.c
+++ 105_lab5.2.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
 
+    scanf("%d",&n);
+
+    if(n%2==0)
+        n=n-1;
+
+    for(;;)
+    {
+        for(j=0; j<n; j++)
+        {
+            if(j==n/2)
+            {
+                printf("*");
+            }
+            else
+                printf("-");
+        }
+    }
+
+
+
+
+
+
+
+}
